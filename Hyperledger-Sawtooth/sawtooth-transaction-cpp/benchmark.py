2a93de16f629903cb0a1ff273d5d6fe960036b4b
Luc Gerrits <lucgerrits@users.noreply.github.com>
2021-04-08 15:34:37 +0200

    save

--- Hyperledger-Sawtooth/sawtooth-transaction-cpp/benchmark.py
+++ Hyperledger-Sawtooth/sawtooth-transaction-cpp/benchmark.py
@@ -5,11 +5,50 @@ import subprocess
 import datetime
 import time
 import pathlib
+import os
 ##################### CONFIG #####################
 
 file_sender_log = "sender_log.log"
 file_stats_log = "stats_log.log"
 
+# config doc: https://sawtooth.hyperledger.org/docs/pbft/nightly/master/configuring-pbft.html
+test_profiles = [
+    # {  # basic test first
+    #     "sender_parameters": {
+    #         "limit": "100",
+    #         "js_nb_parallele": "3",
+    #         "js_wait_time": "1"
+    #     }
+    # },
+    # {
+    #     "sender_parameters": {
+    #         "limit": "5000",
+    #         "js_nb_parallele": "3",
+    #         "js_wait_time": "1"
+    #     }
+    # },
+    {
+        "sender_parameters": {
+            "limit": "10000",
+            "js_nb_parallele": "7",
+            "js_wait_time": "1"
+        }
+    },
+    # {
+    #     "sender_parameters": {
+    #         "limit": "10000",
+    #         "js_nb_parallele": "10",
+    #         "js_wait_time": "1"
+    #     }
+    # },
+    # {
+    #     "sender_parameters": {
+    #         "limit": "10000",
+    #         "js_nb_parallele": "20",
+    #         "js_wait_time": "1"
+    #     }
+    # },
+]
 ##################### DEF GENERAL #####################
 
 
@@ -17,9 +56,10 @@ def log(_str, ts=True, end="\n"):
     now = datetime.datetime.now()
     if ts:
         print("{} | {}".format(now.strftime("%Y-%m-%d %H:%M:%S"), _str), end=end)
+        return "{} | {}".format(now.strftime("%Y-%m-%d %H:%M:%S"), _str)
     else:
         print("{}".format(_str), end=end)
-
+        return "{}".format(_str)
 
 def clear_file(filename):
     text_file = open(filename, "w")
@@ -74,6 +114,8 @@ def start_sender(sender_parameters, test_name):
     # result = subprocess.check_output(
     #     ['docker-compose', '-f', 'docker-compose-sender.yaml.benchmark', 'up', 'sender'], stderr=subprocess.STDOUT)
 
+    subprocess.check_output(['docker-compose', '-f', 'docker-compose-sender.yaml.benchmark', 'down'], stderr=subprocess.STDOUT)
+
     params = "{} {} {}".format(
         sender_parameters["js_nb_parallele"], sender_parameters["js_wait_time"], sender_parameters["limit"])
     set_file_params("docker-compose-sender.yaml.benchmark",
@@ -92,20 +134,32 @@ def start_sender(sender_parameters, test_name):
     append_file(file_sender_log,
                 "\n============= END {} ============\n".format(test_name))
     log("Sleep {} sec for blockchain stabilize".format(20))
-    time.sleep(20)
+    time.sleep(30)
 
 
 def start_stats(test_name):
+
     log("Start stats")
     working_dir = pathlib.Path(__file__).parent.absolute()
-    result = subprocess.check_output(
-        ['python3', 'test_links.py'], cwd=working_dir, stderr=subprocess.STDOUT)
-    log(result.decode("utf-8"), ts=False)
+    append_file(file_stats_log, log(""))
     append_file(file_stats_log,
                 "\n============= {} ============\n".format(test_name))
-    append_file(file_stats_log, result.decode("utf-8"))
+    with open(file_stats_log,"a") as f:
+        subprocess.call(
+            ['python3', 'test_links.py'], cwd=working_dir, stderr=f, stdout=f)
     append_file(file_stats_log,
                 "\n============= END {} ============\n".format(test_name))
+    
+    # log("Start stats")
+    # working_dir = pathlib.Path(__file__).parent.absolute()
+    # result = subprocess.check_output(
+    #     ['python3', 'test_links.py'], cwd=working_dir, stderr=subprocess.STDOUT)
+    # log(result.decode("utf-8"), ts=False)
+    # append_file(file_stats_log,
+    #             "\n============= {} ============\n".format(test_name))
+    # append_file(file_stats_log, result.decode("utf-8"))
+    # append_file(file_stats_log,
+    #             "\n============= END {} ============\n".format(test_name))
     time.sleep(1)
 
 ##################### MAIN #####################
@@ -131,196 +185,11 @@ def main(profiles):
     log("Finishing")
 
     log("Done {} benchmarks".format(nb_profiles))
+    os.system('spd-say "Benchmark finished"')
 
 
-# config doc: https://sawtooth.hyperledger.org/docs/pbft/nightly/master/configuring-pbft.html
-test_profiles = [
-    {  # basic test first
-        "sender_parameters": {
-            "limit": "1000",
-            "js_nb_parallele": "3",
-            "js_wait_time": "1"
-        }
-    },
-    # {  # basic test first
-    #     "sawtooth_parameters": {
-    #         "max_batches_per_block": "100",
-    #         "block_publishing_delay": "1000",
-    #         "idle_timeout": "30000",
-    #         "forced_view_change_interval": "100",
-    #         "view_change_duration": "5000",
-    #         "commit_timeout": "10000"
-    #     },
-    #     "sender_parameters": {
-    #         "limit": "1000",
-    #         "js_nb_parallele": "10",
-    #         "js_wait_time": "3"
-    #     }
-    # },
-    # {
-    #     "sawtooth_parameters": {
-    #         "max_batches_per_block": "100",
-    #         "block_publishing_delay": "1000",
-    #         "idle_timeout": "30000",
-    #         "forced_view_change_interval": "100",
-    #         "view_change_duration": "5000",
-    #         "commit_timeout": "10000"
-    #     },
-    #     "sender_parameters": {
-    #         "limit": "5000",
-    #         "js_nb_parallele": "3",
-    #         "js_wait_time": "1"
-    #     }
-    # },
-    # {
-    #     "sawtooth_parameters": {
-    #         "max_batches_per_block": "100",
-    #         "block_publishing_delay": "1000",
-    #         "idle_timeout": "30000",
-    #         "forced_view_change_interval": "100",
-    #         "view_change_duration": "5000",
-    #         "commit_timeout": "10000"
-    #     },
-    #     "sender_parameters": {
-    #         "limit": "10000",
-    #         "js_nb_parallele": "300",
-    #         "js_wait_time": "100"
-    #     }
-    # },
-    # {
-    #     "sawtooth_parameters": {
-    #         "max_batches_per_block": "200",
-    #         "block_publishing_delay": "1000",
-    #         "idle_timeout": "30000",
-    #         "forced_view_change_interval": "100",
-    #         "view_change_duration": "5000",
-    #         "commit_timeout": "10000"
-    #     },
-    #     "sender_parameters": {
-    #         "limit": "10000",
-    #         "js_nb_parallele": "300",
-    #         "js_wait_time": "100"
-    #     }
-    # },
-    # {
-    #     "sawtooth_parameters": {
-    #         "max_batches_per_block": "500",
-    #         "block_publishing_delay": "1000",
-    #         "idle_timeout": "30000",
-    #         "forced_view_change_interval": "100",
-    #         "view_change_duration": "5000",
-    #         "commit_timeout": "10000"
-    #     },
-    #     "sender_parameters": {
-    #         "limit": "10000",
-    #         "js_nb_parallele": "3",
-    #         "js_wait_time": "1"
-    #     }
-    # },
-    # {
-    #     "sawtooth_parameters": {
-    #         "max_batches_per_block": "1000",
-    #         "block_publishing_delay": "1000",
-    #         "idle_timeout": "30000",
-    #         "forced_view_change_interval": "100",
-    #         "view_change_duration": "5000",
-    #         "commit_timeout": "10000"
-    #     },
-    #     "sender_parameters": {
-    #         "limit": "10000",
-    #         "js_nb_parallele": "3",
-    #         "js_wait_time": "1"
-    #     }
-    # },
-    # {
-    #     "sawtooth_parameters": {
-    #         "max_batches_per_block": "500",
-    #         "block_publishing_delay": "1000",
-    #         "idle_timeout": "30000",
-    #         "forced_view_change_interval": "100",
-    #         "view_change_duration": "5000",
-    #         "commit_timeout": "10000"
-    #     },
-    #     "sender_parameters": {
-    #         "limit": "10000",
-    #         "js_nb_parallele": "3",
-    #         "js_wait_time": "5"
-    #     }
-    # },
-    # {
-    #     "sawtooth_parameters": {
-    #         "max_batches_per_block": "500",
-    #         "block_publishing_delay": "1000",
-    #         "idle_timeout": "30000",
-    #         "forced_view_change_interval": "5",
-    #         "view_change_duration": "5000",
-    #         "commit_timeout": "10000"
-    #     },
-    #     "sender_parameters": {
-    #         "limit": "10000",
-    #         "js_nb_parallele": "3",
-    #         "js_wait_time": "1"
-    #     }
-    # },
-    # {
-    #     "sawtooth_parameters": {
-    #         "max_batches_per_block": "500",
-    #         "block_publishing_delay": "1000",
-    #         "idle_timeout": "30000",
-    #         "forced_view_change_interval": "50",
-    #         "view_change_duration": "5000",
-    #         "commit_timeout": "10000"
-    #     },
-    #     "sender_parameters": {
-    #         "limit": "10000",
-    #         "js_nb_parallele": "3",
-    #         "js_wait_time": "1"
-    #     }
-    # },
-    # {
-    #     "sawtooth_parameters": {
-    #         "max_batches_per_block": "500",
-    #         "block_publishing_delay": "1000",
-    #         "idle_timeout": "30000",
-    #         "forced_view_change_interval": "500",
-    #         "view_change_duration": "5000",
-    #         "commit_timeout": "10000"
-    #     },
-    #     "sender_parameters": {
-    #         "limit": "10000",
-    #         "js_nb_parallele": "3",
-    #         "js_wait_time": "1"
-    #     }
-    # },
-    # {
-    #     "sawtooth_parameters": {
-    #         "max_batches_per_block": "500",
-    #         "block_publishing_delay": "1000",
-    #         "idle_timeout": "30000",
-    #         "forced_view_change_interval": "100",
-    #         "view_change_duration": "5000",
-    #         "commit_timeout": "10000"
-    #     },
-    #     "sender_parameters": {
-    #         "limit": "10000",
-    #         "js_nb_parallele": "3",
-    #         "js_wait_time": "1"
-    #     }
-    # },
-    # {
-    #     "sawtooth_parameters": {
-    #         "max_batches_per_block": "500",
-    #         "block_publishing_delay": "2000",
-    #         "idle_timeout": "60000",
-    #         "forced_view_change_interval": "200",
-    #         "view_change_duration": "10000",
-    #         "commit_timeout": "20000"
-    #     },
-    #     "sender_parameters": {
-    #         "limit": "10000",
-    #         "js_nb_parallele": "3",
-    #         "js_wait_time": "1"
-    #     }
-    # },
-]
-main(test_profiles)
+# main(test_profiles)
+try:
+    main(test_profiles)
+except Exception as e:
+    print( "Error: \n %s" % str(e) )
