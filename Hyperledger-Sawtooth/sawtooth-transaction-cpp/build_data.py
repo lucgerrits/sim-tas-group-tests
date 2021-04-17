ea66e42dd5c76a265bedd925be335e1941125674
Luc Gerrits <lucgerrits@users.noreply.github.com>
2021-04-17 19:43:39 +0200

    add better detection of start and end of tests

--- Hyperledger-Sawtooth/sawtooth-transaction-cpp/build_data.py
+++ Hyperledger-Sawtooth/sawtooth-transaction-cpp/build_data.py
@@ -7,29 +7,42 @@
 import os
 import pandas as pd
 import numpy as np
-# import sys
 import glob
 import pylab as pl
-import re 
-from pathlib import Path 
 from matplotlib import cm
-from scipy.interpolate import make_interp_spline, BSpline
 from scipy.ndimage.filters import gaussian_filter1d
 
 #%%
 #global variables:
 data_path = "./datas"
 final_df = None
-detect_benchmarks_on="commits"
-fields = { #fields (= csv filename) with their corresponding column name
-    "sawtooth_validator.chain.ChainController.block_num": "block_num", 
-    "sawtooth_validator.chain.ChainController.committed_transactions_gauge": "commits", 
-    "sawtooth_validator.executor.TransactionExecutorThread.tp_process_response_count": "tx_exec_rate", 
-    "sawtooth_validator.publisher.BlockPublisher.pending_batch_gauge": "pending_tx", 
-    "sawtooth_validator.chain.ChainController.blocks_considered_count": "blocks_count", 
-    "sawtooth_validator.back_pressure_handlers.ClientBatchSubmitBackpressureHandler.backpressure_batches_rejected_gauge": "reject_rate", 
-    "sawtooth_rest_api.post_batches_count": "rest_api_batch_rate", 
-    "sawtooth_validator.interconnect.Interconnect.send_response_time": "msg_sent_rate", 
+#detect_benchmarks_on="commits"
+#initialization_threshold=1000 #500*2 tx commits for init in our case
+#detect_benchmark_threshold=3000 #commits jump N tx = change of test
+detect_benchmarks_on="block_num"
+
+#5~300 tx blocks for init in our considered_councase:
+initialization_threshold=310
+
+#benchmark ended if consecutive elements are equal.
+#Delete all data between start of detected consecutive element and jump of detect_benchmark_threshold
+#Using 2, stop detected when 2 consecutive elements are strictly equal
+#Note: minimum=2, recommended=4
+detect_benchmark_stop_elements=3
+
+#blocks jump of N detect_benchmarks_on (= change of test)
+detect_benchmark_threshold=50
+
+#fields (= csv filename) with their corresponding column name:
+fields = {
+    "sawtooth_validator.chain.ChainController.block_num": "block_num",
+    "sawtooth_validator.chain.ChainController.committed_transactions_gauge": "commits",
+    "sawtooth_validator.executor.TransactionExecutorThread.tp_process_response_count": "tx_exec_rate",
+    "sawtooth_validator.publisher.BlockPublisher.pending_batch_gauge": "pending_tx",
+    "sawtooth_validator.chain.ChainController.blocks_considered_count": "blocks_count",
+    "sawtooth_validator.back_pressure_handlers.ClientBatchSubmitBackpressureHandler.backpressure_batches_rejected_gauge": "reject_rate",
+    "sawtooth_rest_api.post_batches_count": "rest_api_batch_rate",
+    "sawtooth_validator.interconnect.Interconnect.send_response_time": "msg_sent_rate",
     "sawtooth_validator.interconnect._SendReceive.received_message_count": "msg_receive_rate"
 }
 
@@ -45,8 +58,8 @@ for filename in all_files:
     try:#test if field exists
         test=fields[field_name]
     except:
-        print("Missing CSV file for field: {}".format(field_key))
-        exit()
+        print("Missing CSV file for field: {}".format(field_name))
+        exit(1)
 
     tmp_df[fields[field_name]] = df
     #rename columns
@@ -64,7 +77,7 @@ for field_key in tmp_df.keys():
     final_df = final_df.merge(tmp_df[field_key], on="time", how="left")
 
 #fill NaN with previous value
-final_df = final_df.fillna(method='ffill')
+final_df = final_df.fillna(method='ffill').fillna(0)
 #%%
 #
 # Print available columns
@@ -77,20 +90,56 @@ for col in final_df.columns:
 print("")
 #%%
 #
+# Filter out initialization of the test: only keep data where commits>1000
+#
+#thanks to https://stackoverflow.com/a/27360130/13187605
+final_df = final_df.drop(final_df[(final_df[detect_benchmarks_on] >= 0) & (final_df[detect_benchmarks_on] < initialization_threshold)].index)
+final_df = final_df.reset_index(drop=True)
+
+#detect benchmark stop
+detect_benchmark_stop_elements=4
+col_detect_index=final_df_cols.index(detect_benchmarks_on)
+#def remove_consecutive_elements(mydf):
+start_at_index=-1
+previous_elements=[] #previous value to compare to
+previous=0 #previous value to compare to
+for index, row in final_df.iterrows():
+    if len(previous_elements) > detect_benchmark_stop_elements:
+        previous_elements.pop(0)
+    
+    previous_elements.append(round(row[col_detect_index]))
+    
+    if index > detect_benchmark_stop_elements:
+        #print("{} {} => {}".format(index,previous_elements,np.array(previous_elements).std()))
+        if np.array(previous_elements).std() < 0.1 and start_at_index == -1:
+            #found same consecutive elements !
+            start_at_index=index
+            
+            
+        if abs(row[col_detect_index] - (previous)) > detect_benchmark_threshold and start_at_index != -1:
+            #found jump !
+            final_df = final_df.drop(final_df.index[start_at_index:index])
+            print("Detect benchmark stop from {} to {}".format(start_at_index,index))
+            start_at_index = -1 #can start deleting delete again
+            
+    previous=row[col_detect_index]
+    
+final_df = final_df.reset_index(drop=True)
+#%%
+#
 # Benchmark detection: use to distinguish multiple test in the dataframe
 #
 final_df["test#"]=0
-threshold=3000 #if commits jump N tx = change of test (VERIFY ALLWAYS IF TESTS MATCH ACTUAL TESTS)
+#detect_benchmark_threshold if commits jump N tx = change of test (VERIFY ALLWAYS IF TESTS MATCH ACTUAL TESTS)
 previous=0 #previous value to compare to
-current_test_index=0 #this is will be the nb of tests at the end of the for loop
+number_of_benchmarks=0 #this is will be the nb of tests at the end of the for loop
 col_detect_index=final_df_cols.index(detect_benchmarks_on)
 for index, row in final_df.iterrows():
-    if abs(row[col_detect_index] - previous) > threshold:
-        current_test_index = current_test_index +col_detect_index
-        final_df.loc[index, "test#"] = current_test_index
-    else:
-        final_df.loc[index, "test#"] = current_test_index
-    previous = row[col_detect_index]
+    if abs(row[col_detect_index] - (previous)) > detect_benchmark_threshold:
+        number_of_benchmarks = number_of_benchmarks + col_detect_index
+    previous=row[col_detect_index]
+    final_df.loc[index, "test#"] = number_of_benchmarks
+print("Benchmark detected = {}".format(number_of_benchmarks))
 #end benchmark detection
 
 #%%
@@ -99,36 +148,52 @@ for index, row in final_df.iterrows():
 # (auto color and auto benchmark detection based on "test#" column)
 #
 #color map here:
-cm_subsection = np.linspace(0.0, 1.0, current_test_index+1) 
+cm_subsection = np.linspace(0.0, 1.0, number_of_benchmarks+1)
 colors = [ cm.jet(x) for x in cm_subsection ]
 #self inc on each myplot() call:
-total_plots=0 
+total_plots=0
 #general plot fct to make it simple:
-def myplot(X_colomn_name, Y_colomn_name, title, xlabel, ylabel, smooth = False):
+def myplot(plot_type, X_colomn_name, Y_colomn_name, title, smooth = False):
     global total_plots
     #help on legend placement here: https://stackoverflow.com/a/4701285/13187605
-    fig = pl.figure(total_plots)
+    pl.figure(total_plots)
     ax = pl.subplot(111)
     #temp_tests_list = np.array([])
-    for i in range(0, current_test_index+1):
+    for i in range(0, number_of_benchmarks+1):
         #print lines for each test, using diff colors
         X_values=final_df.loc[final_df['test#'] == i].values[:,final_df_cols.index(X_colomn_name)]
         Y_values=final_df.loc[final_df['test#'] == i].values[:,final_df_cols.index(Y_colomn_name)]
         #np.append(temp_tests_list, Y_values)
         if smooth:
             Y_values= gaussian_filter1d(Y_values, sigma=1) # make more smooth: BE CARFUL
-        pl.plot(X_values,Y_values, color=colors[i], label="Test#{}".format(i))
+        if plot_type == "line":
+            ax.plot(X_values,Y_values, color=colors[i], label="Test#{}".format(i))
+        elif plot_type == "scatter":
+            ax.scatter(X_values,Y_values, color=colors[i], label="Test#{}".format(i))
+        elif plot_type == "bar":
+            if len(X_values) == 0: #fix an value error create by "bar" if empty data
+                X_values= [0]
+                Y_values= [0]
+            ax.bar(X_values, Y_values, color=colors[i], alpha=0.5, label="Test#{}".format(i))
+            # pl.hist(Y_values, color=colors[i], label="Test#{} ({})".format(i, Y_colomn_name))
+        else:
+            print("ERROR: Unknown plot type: {}".format(plot_type))
+            exit(1)
     pl.title(title)
-    pl.xlabel(xlabel)
-    pl.ylabel(ylabel)
+    pl.xlabel(X_colomn_name)
+    pl.ylabel(Y_colomn_name)
     #print(temp_tests_list)
     #print("{} => variance={}".format(title, np.var(temp_tests_list)))
     box = ax.get_position()
-    ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
+    ax.set_position([box.x0, box.y0, box.width * 0.7, box.height])
     ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
     total_plots = total_plots+1
 
-# print(final_df)
+
+#def myplot_tests_merged(plot_type, X_colomn_name, Y_colomn_name, title, smooth = False):
+    #TODO
+
+print(final_df)
 # exit() #for debug
 #%%
 #
@@ -144,7 +209,9 @@ def myplot(X_colomn_name, Y_colomn_name, title, xlabel, ylabel, smooth = False):
 # myplot(1, 7, "commits vs msg sent rate", "commits", "msg sent rate")
 # myplot(1, 8, "commits vs msg receive rate", "commits", "msg receive rate")
 
-myplot("commits", "tx_exec_rate", "Title", "x", "y", True)
+myplot("line", "time", "commits", "commits in time")
+
+myplot("bar","rest_api_batch_rate", "tx_exec_rate", "Title")
 
 
 pl.show()
