cae20ab4b4b06a5e02a835e3a647232b0649a05c
Luc Gerrits <lucgerrits@users.noreply.github.com>
2021-04-17 14:17:27 +0200

    fix influx export for rest api

--- Hyperledger-Sawtooth/sawtooth-transaction-cpp/export_influxdb_data.sh
+++ Hyperledger-Sawtooth/sawtooth-transaction-cpp/export_influxdb_data.sh
@@ -15,7 +15,7 @@ mkdir -p $DATA_PATH
 NOW=$(date +%s%N | cut -b1-13)
 START_TS=1618508327940
 END_TS=1618514773553 #$NOW
-GROUP_BY_TIME="60s" #in seconds
+GROUP_BY_TIME="10s" #in seconds
 timeFilter="time >= ${START_TS}ms and time <= ${END_TS}ms "
 
 #Note:
@@ -23,19 +23,16 @@ timeFilter="time >= ${START_TS}ms and time <= ${END_TS}ms "
 #
 
 ####
-# field="sawtooth_validator.chain.ChainController.block_num"
-# QUERY="SELECT mean(\"value\") FROM \"$field\" WHERE $timeFilter GROUP BY time($GROUP_BY_TIME) fill(none)" #, \"host\"
-# response=$(curl -sS --insecure -u $INFLUX_USER:$INFLUX_PWD -XPOST "$INFLUX_URL/query" --data-urlencode "db=$INFLUX_DB" --data-urlencode "q=$QUERY" -H "Accept: application/csv")
-# echo "$response" > "$DATA_PATH/$field/$i.csv"
+field="sawtooth_validator.chain.ChainController.block_num"
+QUERY="SELECT mean(\"value\") FROM \"$field\" WHERE $timeFilter GROUP BY time($GROUP_BY_TIME) fill(none)" #, \"host\"
+curl -sS --insecure -u $INFLUX_USER:$INFLUX_PWD -XPOST "$INFLUX_URL/query" --data-urlencode "db=$INFLUX_DB" --data-urlencode "q=$QUERY" -H "Accept: application/csv" > "$DATA_PATH/$field.csv"
 ####
 field="sawtooth_validator.chain.ChainController.committed_transactions_gauge"
-QUERY="SELECT mean(\"value\") FROM \"$field\" WHERE $timeFilter GROUP BY time($GROUP_BY_TIME) fill(null)" #, \"host\"
-# echo $QUERY
+QUERY="SELECT mean(\"value\") as mean FROM \"$field\" WHERE $timeFilter GROUP BY time($GROUP_BY_TIME) fill(null)" #, \"host\"
 curl -sS --insecure -u $INFLUX_USER:$INFLUX_PWD -XPOST "$INFLUX_URL/query" --data-urlencode "db=$INFLUX_DB" --data-urlencode "q=$QUERY" -H "Accept: application/csv" > "$DATA_PATH/$field.csv"
 ####
 field="sawtooth_validator.executor.TransactionExecutorThread.tp_process_response_count"
-QUERY="SELECT (non_negative_derivative(percentile(\"count\", 99.9), 10s) /10) as mean FROM \"$field\" WHERE  $timeFilter GROUP BY time($GROUP_BY_TIME) fill(null)" #, \"host\"  (\"response_type\" = 'OK') AND
-# echo $QUERY
+QUERY="SELECT (moving_average(non_negative_derivative(mean(\"count\"), 10s),2) /10) as mean FROM \"$field\" WHERE  $timeFilter GROUP BY time($GROUP_BY_TIME) fill(null)" #, \"host\"  (\"response_type\" = 'OK') AND
 curl -sS --insecure -u $INFLUX_USER:$INFLUX_PWD -XPOST "$INFLUX_URL/query" --data-urlencode "db=$INFLUX_DB" --data-urlencode "q=$QUERY" -H "Accept: application/csv" > "$DATA_PATH/$field.csv"
 ####
 field="sawtooth_validator.publisher.BlockPublisher.pending_batch_gauge"
@@ -51,7 +48,8 @@ QUERY="SELECT (non_negative_derivative(sum(\"value\"), 10s) /10) as mean FROM \"
 curl -sS --insecure -u $INFLUX_USER:$INFLUX_PWD -XPOST "$INFLUX_URL/query" --data-urlencode "db=$INFLUX_DB" --data-urlencode "q=$QUERY" -H "Accept: application/csv" > "$DATA_PATH/$field.csv"
 ####
 field="sawtooth_rest_api.post_batches_count"
-QUERY="SELECT (non_negative_derivative(sum(\"count\"), 10s) /10) as mean FROM \"$field\" WHERE  $timeFilter GROUP BY time($GROUP_BY_TIME) fill(null)" #, \"host\"  (\"response_type\" = 'OK') AND
+# QUERY="SELECT (non_negative_derivative(sum(\"count\"), 10s) /10) as mean FROM \"$field\" WHERE  $timeFilter GROUP BY time($GROUP_BY_TIME) fill(null)" #, \"host\"  (\"response_type\" = 'OK') AND
+QUERY="SELECT ( non_negative_derivative(moving_average(sum(\"count\"), 10), 10s) /10) as mean FROM \"$field\" WHERE  $timeFilter GROUP BY time($GROUP_BY_TIME) fill(null)" #, \"host\"  (\"response_type\" = 'OK') AND
 curl -sS --insecure -u $INFLUX_USER:$INFLUX_PWD -XPOST "$INFLUX_URL/query" --data-urlencode "db=$INFLUX_DB" --data-urlencode "q=$QUERY" -H "Accept: application/csv" > "$DATA_PATH/$field.csv"
 ####
 #unsure here:
