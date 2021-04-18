dc42c8d663d2143837db20dc0365242ca62a4c7b
Luc Gerrits <lucgerrits@users.noreply.github.com>
2021-04-18 17:21:18 +0200

    add export fields

--- Hyperledger-Sawtooth/sawtooth-transaction-cpp/export_influxdb_data.sh
+++ Hyperledger-Sawtooth/sawtooth-transaction-cpp/export_influxdb_data.sh
@@ -13,8 +13,8 @@ rm -rf $DATA_PATH
 mkdir -p $DATA_PATH
 
 NOW=$(date +%s%N | cut -b1-13)
-START_TS=1618508327940
-END_TS=1618514773553 #$NOW
+START_TS=1618672134272
+END_TS=1618678883755 #$NOW
 GROUP_BY_TIME="10s" #in seconds
 timeFilter="time >= ${START_TS}ms and time <= ${END_TS}ms "
 
@@ -24,46 +24,66 @@ timeFilter="time >= ${START_TS}ms and time <= ${END_TS}ms "
 
 ####
 field="sawtooth_validator.chain.ChainController.block_num"
-QUERY="SELECT mean(\"value\") FROM \"$field\" WHERE $timeFilter GROUP BY time($GROUP_BY_TIME) fill(none)" #, \"host\"
-curl -sS --insecure -u $INFLUX_USER:$INFLUX_PWD -XPOST "$INFLUX_URL/query" --data-urlencode "db=$INFLUX_DB" --data-urlencode "q=$QUERY" -H "Accept: application/csv" > "$DATA_PATH/$field.csv"
+name="block_num"
+QUERY="SELECT mean(\"value\") as mean FROM \"$field\" WHERE $timeFilter GROUP BY time($GROUP_BY_TIME) fill(none)" #, \"host\"
+curl -sS --insecure -u $INFLUX_USER:$INFLUX_PWD -XPOST "$INFLUX_URL/query" --data-urlencode "db=$INFLUX_DB" --data-urlencode "q=$QUERY" -H "Accept: application/csv" > "$DATA_PATH/$name.csv"
+####
+field="sawtooth_validator.chain.ChainController.block_num"
+name="block_num_rate"
+QUERY="SELECT (non_negative_derivative(mean(\"value\"), 10s) /10) as mean FROM \"$field\" WHERE $timeFilter GROUP BY time($GROUP_BY_TIME) fill(none)" #, \"host\"
+curl -sS --insecure -u $INFLUX_USER:$INFLUX_PWD -XPOST "$INFLUX_URL/query" --data-urlencode "db=$INFLUX_DB" --data-urlencode "q=$QUERY" -H "Accept: application/csv" > "$DATA_PATH/$name.csv"
 ####
 field="sawtooth_validator.chain.ChainController.committed_transactions_gauge"
+name="commits"
 QUERY="SELECT mean(\"value\") as mean FROM \"$field\" WHERE $timeFilter GROUP BY time($GROUP_BY_TIME) fill(null)" #, \"host\"
-curl -sS --insecure -u $INFLUX_USER:$INFLUX_PWD -XPOST "$INFLUX_URL/query" --data-urlencode "db=$INFLUX_DB" --data-urlencode "q=$QUERY" -H "Accept: application/csv" > "$DATA_PATH/$field.csv"
+curl -sS --insecure -u $INFLUX_USER:$INFLUX_PWD -XPOST "$INFLUX_URL/query" --data-urlencode "db=$INFLUX_DB" --data-urlencode "q=$QUERY" -H "Accept: application/csv" > "$DATA_PATH/$name.csv"
+####
+field="sawtooth_validator.chain.ChainController.committed_transactions_gauge"
+name="commits_rate"
+QUERY="SELECT (non_negative_derivative(mean(\"value\"), 10s) /10) as mean FROM \"$field\" WHERE $timeFilter GROUP BY time($GROUP_BY_TIME) fill(null)" #, \"host\"
+curl -sS --insecure -u $INFLUX_USER:$INFLUX_PWD -XPOST "$INFLUX_URL/query" --data-urlencode "db=$INFLUX_DB" --data-urlencode "q=$QUERY" -H "Accept: application/csv" > "$DATA_PATH/$name.csv"
 ####
 field="sawtooth_validator.executor.TransactionExecutorThread.tp_process_response_count"
+name="tx_exec_rate"
 QUERY="SELECT (moving_average(non_negative_derivative(mean(\"count\"), 10s),2) /10) as mean FROM \"$field\" WHERE  $timeFilter GROUP BY time($GROUP_BY_TIME) fill(null)" #, \"host\"  (\"response_type\" = 'OK') AND
-curl -sS --insecure -u $INFLUX_USER:$INFLUX_PWD -XPOST "$INFLUX_URL/query" --data-urlencode "db=$INFLUX_DB" --data-urlencode "q=$QUERY" -H "Accept: application/csv" > "$DATA_PATH/$field.csv"
+curl -sS --insecure -u $INFLUX_USER:$INFLUX_PWD -XPOST "$INFLUX_URL/query" --data-urlencode "db=$INFLUX_DB" --data-urlencode "q=$QUERY" -H "Accept: application/csv" > "$DATA_PATH/$name.csv"
 ####
 field="sawtooth_validator.publisher.BlockPublisher.pending_batch_gauge"
+name="pending_tx"
 QUERY="SELECT mean(\"value\") FROM \"$field\" WHERE  $timeFilter GROUP BY time($GROUP_BY_TIME) fill(null)" #, \"host\"  (\"response_type\" = 'OK') AND
-curl -sS --insecure -u $INFLUX_USER:$INFLUX_PWD -XPOST "$INFLUX_URL/query" --data-urlencode "db=$INFLUX_DB" --data-urlencode "q=$QUERY" -H "Accept: application/csv" > "$DATA_PATH/$field.csv"
+curl -sS --insecure -u $INFLUX_USER:$INFLUX_PWD -XPOST "$INFLUX_URL/query" --data-urlencode "db=$INFLUX_DB" --data-urlencode "q=$QUERY" -H "Accept: application/csv" > "$DATA_PATH/$name.csv"
 ####
 field="sawtooth_validator.chain.ChainController.blocks_considered_count"
+name="blocks_count"
 QUERY="SELECT mean(\"count\") as mean FROM \"$field\" WHERE  $timeFilter GROUP BY time($GROUP_BY_TIME) fill(null)" #, \"host\"  (\"response_type\" = 'OK') AND
-curl -sS --insecure -u $INFLUX_USER:$INFLUX_PWD -XPOST "$INFLUX_URL/query" --data-urlencode "db=$INFLUX_DB" --data-urlencode "q=$QUERY" -H "Accept: application/csv" > "$DATA_PATH/$field.csv"
+curl -sS --insecure -u $INFLUX_USER:$INFLUX_PWD -XPOST "$INFLUX_URL/query" --data-urlencode "db=$INFLUX_DB" --data-urlencode "q=$QUERY" -H "Accept: application/csv" > "$DATA_PATH/$name.csv"
 ####
 field="sawtooth_validator.back_pressure_handlers.ClientBatchSubmitBackpressureHandler.backpressure_batches_rejected_gauge"
+name="reject_rate"
 QUERY="SELECT (non_negative_derivative(sum(\"value\"), 10s) /10) as mean FROM \"$field\" WHERE  $timeFilter GROUP BY time($GROUP_BY_TIME) fill(null)" #, \"host\"  (\"response_type\" = 'OK') AND
-curl -sS --insecure -u $INFLUX_USER:$INFLUX_PWD -XPOST "$INFLUX_URL/query" --data-urlencode "db=$INFLUX_DB" --data-urlencode "q=$QUERY" -H "Accept: application/csv" > "$DATA_PATH/$field.csv"
+curl -sS --insecure -u $INFLUX_USER:$INFLUX_PWD -XPOST "$INFLUX_URL/query" --data-urlencode "db=$INFLUX_DB" --data-urlencode "q=$QUERY" -H "Accept: application/csv" > "$DATA_PATH/$name.csv"
 ####
 field="sawtooth_rest_api.post_batches_count"
+name="rest_api_batch_rate"
 # QUERY="SELECT (non_negative_derivative(sum(\"count\"), 10s) /10) as mean FROM \"$field\" WHERE  $timeFilter GROUP BY time($GROUP_BY_TIME) fill(null)" #, \"host\"  (\"response_type\" = 'OK') AND
 QUERY="SELECT ( non_negative_derivative(moving_average(sum(\"count\"), 10), 10s) /10) as mean FROM \"$field\" WHERE  $timeFilter GROUP BY time($GROUP_BY_TIME) fill(null)" #, \"host\"  (\"response_type\" = 'OK') AND
-curl -sS --insecure -u $INFLUX_USER:$INFLUX_PWD -XPOST "$INFLUX_URL/query" --data-urlencode "db=$INFLUX_DB" --data-urlencode "q=$QUERY" -H "Accept: application/csv" > "$DATA_PATH/$field.csv"
+curl -sS --insecure -u $INFLUX_USER:$INFLUX_PWD -XPOST "$INFLUX_URL/query" --data-urlencode "db=$INFLUX_DB" --data-urlencode "q=$QUERY" -H "Accept: application/csv" > "$DATA_PATH/$name.csv"
 ####
 #unsure here:
 # field="sawtooth_rest_api.post_batches_validator_time"
+# name="batches_time"
 # QUERY="SELECT (non_negative_derivative(mean(\"sum\"), 10s) /10) as mean FROM \"$field\" WHERE  $timeFilter GROUP BY time($GROUP_BY_TIME) fill(null)" #, \"host\"  (\"response_type\" = 'OK') AND
-# curl -sS --insecure -u $INFLUX_USER:$INFLUX_PWD -XPOST "$INFLUX_URL/query" --data-urlencode "db=$INFLUX_DB" --data-urlencode "q=$QUERY" -H "Accept: application/csv" > "$DATA_PATH/$field.csv"
+# curl -sS --insecure -u $INFLUX_USER:$INFLUX_PWD -XPOST "$INFLUX_URL/query" --data-urlencode "db=$INFLUX_DB" --data-urlencode "q=$QUERY" -H "Accept: application/csv" > "$DATA_PATH/$name.csv"
 ####
 field="sawtooth_validator.interconnect.Interconnect.send_response_time"
+name="msg_sent_rate"
 QUERY="SELECT (non_negative_derivative(sum(\"count\"), 10s) /10) as mean FROM \"$field\" WHERE  $timeFilter GROUP BY time($GROUP_BY_TIME) fill(null)" #, \"host\"  (\"response_type\" = 'OK') AND
-curl -sS --insecure -u $INFLUX_USER:$INFLUX_PWD -XPOST "$INFLUX_URL/query" --data-urlencode "db=$INFLUX_DB" --data-urlencode "q=$QUERY" -H "Accept: application/csv" > "$DATA_PATH/$field.csv"
+curl -sS --insecure -u $INFLUX_USER:$INFLUX_PWD -XPOST "$INFLUX_URL/query" --data-urlencode "db=$INFLUX_DB" --data-urlencode "q=$QUERY" -H "Accept: application/csv" > "$DATA_PATH/$name.csv"
 ####
 field="sawtooth_validator.interconnect._SendReceive.received_message_count"
+name="msg_receive_rate"
 QUERY="SELECT (non_negative_derivative(sum(\"count\"), 10s) /10) as mean FROM \"$field\" WHERE  $timeFilter GROUP BY time($GROUP_BY_TIME) fill(null)" #, \"host\"  (\"response_type\" = 'OK') AND
-curl -sS --insecure -u $INFLUX_USER:$INFLUX_PWD -XPOST "$INFLUX_URL/query" --data-urlencode "db=$INFLUX_DB" --data-urlencode "q=$QUERY" -H "Accept: application/csv" > "$DATA_PATH/$field.csv"
+curl -sS --insecure -u $INFLUX_USER:$INFLUX_PWD -XPOST "$INFLUX_URL/query" --data-urlencode "db=$INFLUX_DB" --data-urlencode "q=$QUERY" -H "Accept: application/csv" > "$DATA_PATH/$name.csv"
 ####
 
 echo "*******"
