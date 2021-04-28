794f090e176d674a3cac839924e3ff0a899aa068
Luc Gerrits <lucgerrits@users.noreply.github.com>
2021-04-28 11:12:42 +0200

    start building cartp in rust

---
+++ Hyperledger-Sawtooth/cartp-rust/fresh-start.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+# VALIDATORDATAPATH="./sawtooth-data/validator-data/"
+# VALIDATORDATAPATH="/media/100GB/validator-data"
+
+#use env:
+export $(grep -v '^#' .env | xargs -d '\n')
+
+do_build () {
+    if [ ! -d ./sawtooth-sdk-rust ]; then
+        echo -n "Can't find sawtooth-sdk-rust"
+        echo "=> Cloning project"
+        git clone --depth 1 --branch v0.5.0 https://github.com/hyperledger/sawtooth-sdk-rust.git
+        echo "Done"
+    fi
+
+    echo -n "Adding cartp files in sawtooth-sdk-rust"
+    cp -rf ./cartp-data/bin/cartp-tp-rust ./sawtooth-sdk-rust/bin/
+    echo -n " ="
+    cp -rf ./cartp-data/cartp_rust ./sawtooth-sdk-rust/examples/
+    echo " => OK"
+
+
+    echo "Delete validator data"
+    if [ ! -d $VALIDATORDATAPATH ]; then
+        du -sh $VALIDATORDATAPATH
+    fi
+    rm -rf $VALIDATORDATAPATH
+    rm -rf ./sawtooth-data/pbft-shared
+    echo "Done"
+
+    echo "make validator data dir"
+    mkdir -p $VALIDATORDATAPATH
+    mkdir -p $VALIDATORDATAPATH/v0
+    mkdir -p $VALIDATORDATAPATH/v1
+    mkdir -p $VALIDATORDATAPATH/v2
+    mkdir -p $VALIDATORDATAPATH/v3
+    mkdir -p $VALIDATORDATAPATH/v4
+    mkdir -p ./sawtooth-data/pbft-shared
+    echo "Done"
+
+
+    sudo chown 104:104 -R ./stats-data/grafana-data
+}
+
+
+
+read -p "Are you sure to build (overwrite) cartp into sawtooth-sdk-python? [Yy]" -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    echo "===================Start======================"
+    do_build
+    echo "===================Done======================"
+else
+    echo "===================Stop======================"
+fi
