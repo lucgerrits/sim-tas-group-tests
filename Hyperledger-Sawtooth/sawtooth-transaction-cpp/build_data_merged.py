be08ea1afa730fc41e15a2081d62a06549367577
Luc Gerrits <lucgerrits@users.noreply.github.com>
2021-05-12 14:13:06 +0200

    big test data ok, todo: add 24nodes results

--- Hyperledger-Sawtooth/sawtooth-transaction-cpp/build_data_merged.py
+++ Hyperledger-Sawtooth/sawtooth-transaction-cpp/build_data_merged.py
@@ -11,18 +11,31 @@ import numpy as np
 import glob
 import pylab as pl
 from matplotlib import cm
-from scipy.ndimage.filters import gaussian_filter1d
+# from scipy.ndimage.filters import gaussian_filter1d
+from scipy.signal import butter, lfilter, freqz
+import scipy.fftpack
 from datetime import datetime
 pl.style.use('science')
 #pl.style.use('ieee')
+
+#From: https://stackoverflow.com/a/25192640/13187605
+def butter_lowpass(cutoff, fs, order=5):
+    nyq = 0.5 * fs
+    normal_cutoff = cutoff / nyq
+    b, a = butter(order, normal_cutoff, btype='low', analog=False)
+    return b, a
+
+def butter_lowpass_filter(data, cutoff, fs, order=5):
+    b, a = butter_lowpass(cutoff, fs, order=order)
+    y = lfilter(b, a, data)
+    return y
 
 #%%
 #global variables:
 data_path = "./datas_csv/"
 conf_figsize=(20, 10)
 # conf_figsize=(6.4, 4.8)
-merge_on="time"
-filter_out_all_with_elements=["rust|bpb_100", "rust|bpb_50"] #"40tps", "17tps", "25tps", "30tps"
+filter_out_all_with_elements=["18_nodes"] #["50tps|6_nodes", "40tps|6_nodes", "30tps|6_nodes", "50tps|12_nodes", "40tps|12_nodes", "30tps|12_nodes"] #"50tps", "40tps", "30tps"
 filter_out_reverse=True
 #%%
 #
@@ -36,31 +49,22 @@ for filename in all_files:
     
     all_df[field_name] = df
 
+    # #https://stackoverflow.com/a/53380374/13187605
+    # keep_same = {'time'} #keep time column 
+    # all_df[field_name].columns = ['{}{}'.format(c, '' if c in keep_same else '_' + field_name) for c in df.columns]
+
 print(all_df[next(iter(all_df))].columns.values)
 #%%
 #
-# Merge data into one table
+# Get highest index
 #
 
+longest_data_length=0
 all_df_iter=iter(all_df)
-tmp=False #next(all_df_iter)
-#tmp2={}
 for field_key in all_df_iter:
-    #https://stackoverflow.com/a/53380374/13187605
-    keep_same = {'time'}
-    all_df[field_key].columns = ['{}{}'.format(c, '' if c in keep_same else '_' + field_key) for c in df.columns]
-    if not tmp:
-        final_df = all_df[field_key] #init with commits
-        tmp=True
-        continue
-    #merge tables
-    #help, see https://pandas.pydata.org/docs/user_guide/merging.html#brief-primer-on-merge-methods-relational-algebra
-    final_df = pd.merge(final_df, all_df[field_key], left_index=True, right_index=True)
-
-#reorder the dict for nice graphs
-final_df = final_df.sort_index(axis = 1)
-
-
+    if longest_data_length< len(all_df[field_key]):
+        longest_data_length = len(all_df[field_key])
+        
 #%%
 
 #Format for latex
@@ -73,88 +77,118 @@ def filter_out_all_with(text):
             return True if filter_out_reverse else False
     return False if filter_out_reverse else True
 #%%
+#
+# Plot fft
+#
 
-#not ("20tps|12_nodes" in col)
+# fig, axs = pl.subplots(figsize=conf_figsize)
+# show="commits_rate"
+# all_df_iter=iter(all_df)
+# for field_name in all_df_iter:
+#     if filter_out_all_with(field_name):
+#         # Number of samplepoints
+#         N = len(all_df[field_name][show].values) - 1
+#         # sample spacing
+#         T = 10 #group by 10s in influx #1.0 / 10
+
+#         yf = scipy.fftpack.fft(all_df[field_name][show].values)
+#         xf = scipy.fftpack.fftfreq(N, T)[:N//2]
+
+#         axs.plot(xf, 2.0/N * np.abs(yf[0:N//2]), label=formatStrLatex("{}".format(field_name.replace(show+'_',''))))
+#         break
+# axs.set_title(formatStrLatex("FFT {}".format(show)))
+# axs.legend()
+
+#%%
+
+# fig, axs = pl.subplots(2,1,figsize=conf_figsize)
+# show="commits_rate"
+# all_df_iter=iter(all_df)
+# for field_name in all_df_iter:
+#     if filter_out_all_with(field_name):
+#         axs[0].plot(all_df[field_name].index.values*longest_data_length/len(all_df[field_name]), butter_lowpass_filter(all_df[field_name][show].values, 0.008, 1/10, 2), label=formatStrLatex("{}".format(field_name.replace(show+'_',''))))
+#         axs[0].set_title(formatStrLatex("{}".format(show)))
+#         axs[0].legend()
+   
+# show="commits_rate"
+# all_df_iter=iter(all_df)
+# for field_name in all_df_iter:
+#     if filter_out_all_with(field_name):
+#         axs[1].plot(all_df[field_name].index.values*longest_data_length/len(all_df[field_name]), all_df[field_name][show].values, label=formatStrLatex("{}".format(field_name.replace(show+'_',''))))
+#         axs[1].set_title(formatStrLatex("{}".format(show)))
+#         axs[1].legend()
+
+
+#%%
+
 
 fig, axs = pl.subplots(2,2,figsize=conf_figsize)
 show="commits_rate"
-for col in final_df.columns:
-    if show in col and filter_out_all_with(col):
-        axs[0][0].plot(final_df[col].values, label=formatStrLatex("{}".format(col.replace(show+'_',''))))
-#        print(final_df[col].shape)
-axs[0][0].set_title(formatStrLatex("{}".format(show)))
-axs[0][0].legend()
-
+all_df_iter=iter(all_df)
+for field_name in all_df_iter:
+    if filter_out_all_with(field_name):
+        axs[0][0].plot(all_df[field_name].index.values*longest_data_length/len(all_df[field_name]), all_df[field_name][show].values, label=formatStrLatex("{}".format(field_name.replace(show+'_',''))))
+        axs[0][0].set_title(formatStrLatex("{}".format(show)))
+        axs[0][0].legend()
 
 show="pending_tx_rate"
-for col in final_df.columns:
-    if show in col and filter_out_all_with(col):
-        axs[1][0].plot(final_df[col].values, label=formatStrLatex("{}".format(col.replace(show+'_',''))))
-
-#axs[1][0].set_yscale('log')
-axs[1][0].set_title(formatStrLatex("{}".format(show)))
-axs[1][0].legend()
-
+all_df_iter=iter(all_df)
+for field_name in all_df_iter:
+    if filter_out_all_with(field_name):
+        axs[1][0].plot(all_df[field_name].index.values*longest_data_length/len(all_df[field_name]), all_df[field_name][show].values, label=formatStrLatex("{}".format(field_name.replace(show+'_',''))))
+        axs[1][0].set_title(formatStrLatex("{}".format(show)))
+        axs[1][0].legend()
 
 show="tx_exec_rate"
-for col in final_df.columns:
-    if show in col and filter_out_all_with(col):
-        axs[1][1].plot(final_df[col].values, label=formatStrLatex("{}".format(col.replace(show+'_',''))))
-axs[1][1].set_title(formatStrLatex("{}".format(show)))
-axs[1][1].legend()
+all_df_iter=iter(all_df)
+for field_name in all_df_iter:
+    if filter_out_all_with(field_name):
+        axs[1][1].plot(all_df[field_name].index.values*longest_data_length/len(all_df[field_name]), all_df[field_name][show].values, label=formatStrLatex("{}".format(field_name.replace(show+'_',''))))
+        axs[1][1].set_title(formatStrLatex("{}".format(show)))
+        axs[1][1].legend()
 
 show="reject_rate"
-for col in final_df.columns:
-    if show in col and filter_out_all_with(col):
-        axs[0][1].plot(final_df[col].values, label=formatStrLatex("{}".format(col.replace(show+'_',''))))
-axs[0][1].set_title(formatStrLatex("{}".format(show)))
-axs[0][1].legend()
+all_df_iter=iter(all_df)
+for field_name in all_df_iter:
+    if filter_out_all_with(field_name):
+        axs[0][1].plot(all_df[field_name].index.values*longest_data_length/len(all_df[field_name]), all_df[field_name][show].values, label=formatStrLatex("{}".format(field_name.replace(show+'_',''))))
+        axs[0][1].set_title(formatStrLatex("{}".format(show)))
+        axs[0][1].legend()
 
 
 
 fig, axs = pl.subplots(2,2,figsize=conf_figsize)
 show="rest_api_batch_rate"
-for col in final_df.columns:
-    if show in col and filter_out_all_with(col):
-        axs[0][0].plot(final_df[col].values, label=formatStrLatex("{}".format(col.replace(show+'_',''))))
-#        print(final_df[col].shape)
-axs[0][0].set_title(formatStrLatex("{}".format(show)))
-axs[0][0].legend()
-
+all_df_iter=iter(all_df)
+for field_name in all_df_iter:
+    if filter_out_all_with(field_name):
+        axs[0][0].plot(all_df[field_name].index.values*longest_data_length/len(all_df[field_name]), all_df[field_name][show].values, label=formatStrLatex("{}".format(field_name.replace(show+'_',''))))
+        axs[0][0].set_title(formatStrLatex("{}".format(show)))
+        axs[0][0].legend()
 
 show="block_num_rate"
-for col in final_df.columns:
-    if show in col and filter_out_all_with(col):
-        axs[0][1].plot(final_df[col].values, label=formatStrLatex("{}".format(col.replace(show+'_',''))))
-
-#axs[1][0].set_yscale('log')
-axs[0][1].set_title(formatStrLatex("{}".format(show)))
-axs[0][1].legend()
+all_df_iter=iter(all_df)
+for field_name in all_df_iter:
+    if filter_out_all_with(field_name):
+        axs[1][0].plot(all_df[field_name].index.values*longest_data_length/len(all_df[field_name]), all_df[field_name][show].values, label=formatStrLatex("{}".format(field_name.replace(show+'_',''))))
+        axs[1][0].set_title(formatStrLatex("{}".format(show)))
+        axs[1][0].legend()
 
 show="tx_in_process_rate"
-for col in final_df.columns:
-   if show in col and filter_out_all_with(col):
-       axs[1][0].plot(final_df[col].values, label=formatStrLatex("{}".format(col.replace(show+'_',''))))
-axs[1][0].set_title(formatStrLatex("{}".format(show)))
-axs[1][0].legend()
+all_df_iter=iter(all_df)
+for field_name in all_df_iter:
+    if filter_out_all_with(field_name):
+        axs[1][1].plot(all_df[field_name].index.values*longest_data_length/len(all_df[field_name]), all_df[field_name][show].values, label=formatStrLatex("{}".format(field_name.replace(show+'_',''))))
+        axs[1][1].set_title(formatStrLatex("{}".format(show)))
+        axs[1][1].legend()
 
-
-#show="msg_receive_rate"
-#for col in final_df.columns:
-#    if show in col and filter_out_all_with(col):
-#        axs[3][1].plot(final_df[col].values, label=formatStrLatex("{}".format(col.replace(show+'_',''))))
-#axs[3][1].set_title(formatStrLatex("{}".format(show)))
-#axs[3][1].legend()
-
-
-#individual plot
-#fig, axs = pl.subplots(1,1,figsize=conf_figsize)
-#show="msg_"
-#for col in final_df.columns:
-#    if show in col and filter_out_all_with(col):
-#        axs.plot(final_df[col].values, label=formatStrLatex("{}".format(col.replace(show+'_',''))))
-#axs.set_title(formatStrLatex("{}".format(show)))
-#axs.legend()
+show="commits_tot"
+all_df_iter=iter(all_df)
+for field_name in all_df_iter:
+    if filter_out_all_with(field_name):
+        axs[0][1].plot(all_df[field_name].index.values*longest_data_length/len(all_df[field_name]), all_df[field_name][show].values, label=formatStrLatex("{}".format(field_name.replace(show+'_',''))))
+        axs[0][1].set_title(formatStrLatex("{}".format(show)))
+        axs[0][1].legend()
 
 
 pl.show()
---
+++ Hyperledger-Sawtooth/sawtooth-transaction-cpp/export_big_tests.sh
@@ -0,0 +1,112 @@
+#!/bin/bash
+
+echo "Start compiling all results"
+
+echo "4_nodes"
+
+# ./export_influxdb_data.sh 1620404688891 1620410092773
+# ./build_data.py "50tps|4_nodes"
+
+# ./export_influxdb_data.sh 1620411185729 1620416273904
+# ./build_data.py "40tps|4_nodes"
+
+# ./export_influxdb_data.sh 1620417304048 1620423018924
+# ./build_data.py "30tps|4_nodes"
+
+# ./export_influxdb_data.sh 1620423793701 1620430313000
+# ./build_data.py "25tps|4_nodes"
+
+# ./export_influxdb_data.sh 1620430589240 1620436519224
+# ./build_data.py "20tps|4_nodes"
+
+# ./export_influxdb_data.sh 1620437625328 1620444315456
+# ./build_data.py "15tps|4_nodes"
+
+# ./export_influxdb_data.sh 1620445173164 1620452631066
+# ./build_data.py "10tps|4_nodes"
+
+# ./export_influxdb_data.sh 1620453501416 1620464019038
+# ./build_data.py "5tps|4_nodes"
+
+echo "6_nodes"
+
+# ./export_influxdb_data.sh 1620465457345 1620471060750
+# ./build_data.py "50tps|6_nodes"
+
+# ./export_influxdb_data.sh 1620472258000 1620477893158
+# ./build_data.py "40tps|6_nodes"
+
+# ./export_influxdb_data.sh 1620478982503 1620484377276
+# ./build_data.py "30tps|6_nodes"
+
+# ./export_influxdb_data.sh 1620485331946 1620491271415
+# ./build_data.py "25tps|6_nodes"
+
+# ./export_influxdb_data.sh 1620492141336 1620498111828
+# ./build_data.py "20tps|6_nodes"
+
+# ./export_influxdb_data.sh 1620499255000 1620505601129
+# ./build_data.py "15tps|6_nodes"
+
+# ./export_influxdb_data.sh 1620506730389 1620514523880
+# ./build_data.py "10tps|6_nodes"
+
+# ./export_influxdb_data.sh 1620515427481 1620525884540
+# ./build_data.py "5tps|6_nodes"
+
+echo "12_nodes"
+
+# ./export_influxdb_data.sh 1620666660360 1620671832029
+# ./build_data.py "50tps|12_nodes"
+
+# ./export_influxdb_data.sh 1620672745541 1620678355000
+# ./build_data.py "40tps|12_nodes"
+
+# ./export_influxdb_data.sh 1620678978272 1620684924000
+# ./build_data.py "30tps|12_nodes"
+
+# ./export_influxdb_data.sh 1620685703212 1620691829000
+# ./build_data.py "25tps|12_nodes"
+
+# ./export_influxdb_data.sh 1620692457285 1620698550328
+# ./build_data.py "20tps|12_nodes"
+
+# ./export_influxdb_data.sh 1620699487718 1620706203000
+# ./build_data.py "15tps|12_nodes"
+
+# ./export_influxdb_data.sh 1620707154000 1620714680792
+# ./build_data.py "10tps|12_nodes"
+
+# ./export_influxdb_data.sh 1620715659236 1620726568000
+# ./build_data.py "5tps|12_nodes"
+
+
+echo "18_nodes"
+
+# ./export_influxdb_data.sh 1620727892642 1620733389942
+# ./build_data.py "50tps|18_nodes"
+
+# ./export_influxdb_data.sh 1620734187981 1620739836837
+# ./build_data.py "40tps|18_nodes"
+
+# ./export_influxdb_data.sh 1620740574983 1620744895549
+# ./build_data.py "30tps|18_nodes"
+
+# ./export_influxdb_data.sh 1620748889084 1620755081477
+# ./build_data.py "25tps|18_nodes"
+
+# ./export_influxdb_data.sh 1620755879713 1620762109968
+# ./build_data.py "20tps|18_nodes"
+
+# ./export_influxdb_data.sh 1620762996465 1620769849673
+# ./build_data.py "15tps|18_nodes"
+
+# ./export_influxdb_data.sh 1620770574156 1620778328072
+# ./build_data.py "10tps|18_nodes"
+
+# ./export_influxdb_data.sh 1620779257773 1620790566000
+# ./build_data.py "5tps|18_nodes"
+
+echo "24_nodes"
+
+echo "End compiling all results"
