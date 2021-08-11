ed082c9ca6ac8f673bd06d92885a88577a00f003
Cyril Naves <cyril-naves.samuel@etu.univ-cotedazur.fr>
2021-08-11 12:16:38 +0200

    added proper rlp encoding

--- Ethereum/Besu/besu_ibft1/rancher-v2.4.10/login.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-yes | ./rancher login https://rancher.unice.cust.tasfrance.com/v3 --token $1
--- Ethereum/besu/deploy-ethereum.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd ./rancher-v2.4.10/
-
-echo "Load Deployments"
-./rancher kubectl create -f ../ethereum-kube.yaml --validate=false
-
-echo "Done"
