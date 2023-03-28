2bee252fb1926eb48e6ca59cd8482858052ca352
Luc Gerrits <luc.gerrits@univ-cotedazur.fr>
2023-03-28 16:49:35 +0200

    ipfs OK

---
+++ Substrate/coins-2023/cloud-deployments/deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+cd "$(dirname "$0")"
+
+#include the config file:
+chmod +x config.sh
+source config.sh
+
+cd rancher-v2.4.10/
+
+#$1 is the rancher login token
+
+./login.sh $1
+
+
+echo "Deploying IPFS"
+
+./rancher kubectl -n $NAMESPACE apply -f ../ipfs.yaml
